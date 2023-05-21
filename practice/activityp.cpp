a28f25efefe4d40c2faa1cf8e1c34d7aa87b70c3
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-05-22 04:23:59 +0530

    late night

--- practice/activityp.cpp
+++ practice/activityp.cpp
@@ -4,22 +4,23 @@ class Activity{
     public:
     int start, finish; 
 };
-bool compare( Activity s1 , Activity s2){
-    return (s1.finish < s2.finish);
+bool compare(Activity s1, Activity s2){
+    return (s1.finish < s2.finish); 
 }
-void print(Activity arr[], int n){
+void print( Activity arr[], int n){
     sort(arr, arr + n, compare);
+
+    cout << "Maximum number of activites are : \n";
     int i = 0;
-    cout << " (" << arr[i].start << "," << arr[i].finish << ")";
-    for(int j = 0 ; j < n; j++){
+    cout << "(" << arr[i].start << "," << arr[i].finish << ")";
+    for (int j = 0; j < n; j++){
         if(arr[j].start >= arr[i].finish){
             cout << "(" << arr[j].start << "," << arr[j].finish << ")";
-            i = j;
         }
     }
 }
 int main(){
-    Activity arr[] = {{3, 4}, {5, 7}, {8, 9}, {5, 9}, {1, 2}, {0, 2}};
+    Activity arr[] = {{1, 3}, {2, 5}, {3, 4}, {4, 7}, {5, 10}, {6, 9}, {7, 11}, {8, 13}, {9, 12}, {12, 14}};
     int n = sizeof(arr) / sizeof(arr[0]);
     print(arr, n);
     return 0;
