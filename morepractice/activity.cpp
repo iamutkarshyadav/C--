e2c28b78bddf72f9a0a09f5b05ba750e1e78ff67
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-05-23 05:22:23 +0530

    0522

--- morepractice/activity.cpp
+++ morepractice/activity.cpp
@@ -2,27 +2,29 @@
 using namespace std; 
 class Activity{
     public:
-        int start, finish;
+        int start, final;
 };
-bool compare(Activity s1, Activity s2){
-        return (s1.finish < s2.finish);
-}
+ bool compare(Activity s1, Activity s2){
+        return (s1.final < s2.final);
+ }
  void print(Activity arr[], int n){
         sort(arr, arr + n, compare);
         int i = 0;
-        cout << "The maximum number of the activities \n";
-        cout << "{" << arr[i].start << "," << arr[i].finish << "}";
+        cout << "Maximum number of activites are : \n";
+        cout << "(" << arr[i].start << "," << arr[i].final << ")";
         for (int j = 0; j < n; j++){
-            if(arr[j].start >= arr[i].finish){
-                cout << "{" << arr[j].start << "," << arr[j].finish << "}";
+            if (arr[j].start >= arr[i].final){
+                cout << "(" << arr[j].start << "," << arr[j].start << ")";
                 i = j;
-          }
-     }
+            }
+        }
  }
- int main()
- {
-     Activity arr[] = {{3, 4}, {5, 7}, {8, 9}, {5, 9}, {1, 2}, {0, 2}};
-     int n = sizeof(arr) / sizeof(arr[0]);
-     print(arr, n);
-     return 0;
+  
+
+int main()
+{
+    Activity arr[] = {{3, 4}, {5, 7}, {8, 9}, {5, 9}, {1, 2}, {0, 2}};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    print(arr, n);
+    return 0;
  }
